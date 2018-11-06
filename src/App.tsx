b3e947b12bb437a0618850f570132b6bcdb57364
Matt Unhjem <munhjem@renttherunway.com>
2018-11-05 20:08:43 -0500

    added: styles, readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 
+import "./App.css";
+
 type Person = {
   name: string;
   height: string;
@@ -19,25 +21,58 @@ type Person = {
   url: string;
 };
 
+type Planet = {
+  name: string;
+  rotation_period: string;
+  orbital_period: string;
+  diameter: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  surface_water: string;
+  population: string;
+  residents: string[];
+  films: string[];
+  created: Date;
+  edited: Date;
+  url: string;
+};
+
+interface Props {}
+
 interface State {
-  singlePerson: Person | Person[];
+  singlePerson: Person;
+  singlePlanet: Planet;
   people: Person[];
+  planets: Planet[];
 }
 
-class App extends React.Component<{}, State> {
+class App extends React.Component<Props, State> {
   public readonly state = {
     singlePerson: {} as Person,
-    people: []
+    singlePlanet: {} as Planet,
+    people: [],
+    planets: []
   };
+
   public componentDidMount(): void {
     this.getPeople();
+    this.getPlanets();
   }
 
-  public getPerson(userId: number) {
+  public getPerson(userId: number): void {
     fetch(`https://swapi.co/api/people/${userId}/`)
       .then(data => data.json())
       .then(
         (data: Person): void => this.setState(() => ({ singlePerson: data }))
+      );
+  }
+
+  public getPlanet(planetId: number): void {
+    fetch(`https://swapi.co/api/planets/${planetId}/`)
+      .then(data => data.json())
+      .then(
+        (data: Planet): void => this.setState(() => ({ singlePlanet: data }))
       );
   }
 
@@ -47,18 +82,41 @@ class App extends React.Component<{}, State> {
       .then(data => this.setState({ people: data.results }));
   }
 
+  public getPlanets(): void {
+    fetch(`https://swapi.co/api/planets/`)
+      .then(data => data.json())
+      .then(data => this.setState({ planets: data.results }));
+  }
+
   public render() {
     return (
-      <div style={{ padding: "20px" }}>
-        {this.state.people.length > 0 &&
-          this.state.people.map((person: Person, i) => (
-            <button key={i} onClick={() => this.getPerson(i + 1)}>
-              {person.name}
-            </button>
-          ))}
-        <pre className="App">
-          {JSON.stringify(this.state.singlePerson, null, " ")}
-        </pre>
+      <div style={{ padding: "20px", display: "flex", width: "100%" }}>
+        <div
+          style={{ border: "5px solid blue", padding: "10px", flex: "0 0 50%" }}
+        >
+          {this.state.people.length > 0 &&
+            this.state.people.map((person: Person, i) => (
+              <button key={i} onClick={() => this.getPerson(i + 1)}>
+                {person.name}
+              </button>
+            ))}
+          <pre className="App">
+            {JSON.stringify(this.state.singlePerson, null, " ")}
+          </pre>
+        </div>
+        <div
+          style={{ border: "5px solid blue", padding: "10px", flex: "0 0 50%" }}
+        >
+          {this.state.planets.length > 0 &&
+            this.state.planets.map((planet: Planet, i) => (
+              <button key={i} onClick={() => this.getPlanet(i + 1)}>
+                {planet.name}
+              </button>
+            ))}
+          <pre className="App">
+            {JSON.stringify(this.state.singlePlanet, null, " ")}
+          </pre>
+        </div>
       </div>
     );
   }
